180e35c63716d8a46ea596519495573ca57ade9e
Troy Andrada <christiantroyandrada@gmail.com>
2022-06-06 12:15:31 +0800

    added simple JS on website button recursion click LOL

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const web_button = document.getElementById("website");
+web_button.addEventListener("click", function(){
+    alert("This is a recursion if you already can tell. Please click ok to continue."); //recursion if website button is clicked
+    window.location = "https://christiantroyandrada.github.io/personal-page/"
+});
